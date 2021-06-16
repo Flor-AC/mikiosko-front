4db5398417ec7c08b28f3492db95302e6b93a361
Flor-AC <florac206@gmail.com>
2021-06-15 19:54:28 -0500

    Se agregaron las pestañas de la carpeta Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RegistrarComponent} from "./Login/registrar/registrar.component";
+import {PrincipalComponent} from "./Login/principal/principal.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'registrar', component: RegistrarComponent},
+  {path: 'ingresar', component: PrincipalComponent},
+  {path: '', component: PrincipalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
